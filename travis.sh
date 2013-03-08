436fef2dd65317abe04253a5004c2e8f4904dc25
Nathaniel Tabernero <nathaniel.tabernero@nextcentury.com>
2013-03-07 21:21:18 -0500

    ozp-119 fixing build

---
+++ travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mvn install 2>&1 | grep -v -E "^Downloading\:.*$" | cut -c -80
+
+if [ ${PIPESTATUS[0]} -ne "0" ];
+then
+    echo "Build failed with Error: ${PIPESTATUS[0]}";
+    exit 1;
+else
+    echo "Build completed successfully!";
+fi
